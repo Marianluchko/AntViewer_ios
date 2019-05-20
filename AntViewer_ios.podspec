e277adfe21f7b118da2820469bb86995eba566c4
Mykola Vaniurskyi <mv@leobit.co>
2019-05-20 22:54:11 +0300

    Removed SDWebImage from pod

--- AntViewer_ios.podspec
+++ AntViewer_ios.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'AntViewer_ios'
-  s.version          = '0.2.4'
+  s.version          = '0.2.5'
   s.summary          = 'AntViewer provides to users possibility to watch streams and use chat and polls'
 
 # This description is used to generate tags and improve search results.
@@ -29,7 +29,7 @@ Pod::Spec.new do |s|
 
   s.platform     = :ios, "11.3"
   s.source_files = 'AntViewer_ios/Classes/**/*.{swift}'
-  s.ios.vendored_frameworks = 'AntViewer_ios/MyFrameworks/AntViewerExt.framework', 'AntViewer_ios/MyFrameworks/Lottie.framework'
+  s.ios.vendored_frameworks = 'AntViewer_ios/MyFrameworks/AntViewerExt.framework', 'AntViewer_ios/MyFrameworks/Lottie.framework', 'ntViewer_ios/MyFrameworks/SDWebImage.framework'
 
   s.resources = 'AntViewer_ios/Classes/**/*.{storyboard,xib,plist}'
   s.resource_bundles = {
@@ -43,6 +43,6 @@ Pod::Spec.new do |s|
   s.dependency 'Firebase/Core', '~> 5.11'
   s.dependency 'Firebase/Database', '~> 5.11'
   #s.dependency 'lottie-ios', '~> 2.5.3'
-  s.dependency 'SDWebImage', '~> 5.0'
+  #s.dependency 'SDWebImage', '~> 5.0'
   s.swift_version = "5"
 end
--- Example/Pods/Target Support Files/Pods-AntViewer_ios_Example/Pods-AntViewer_ios_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-AntViewer_ios_Example/Pods-AntViewer_ios_Example-frameworks.sh
@@ -164,8 +164,6 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${PODS_ROOT}/../../AntViewer_ios/MyFrameworks/AntViewerExt.framework"
   install_framework "${PODS_ROOT}/../../AntViewer_ios/MyFrameworks/Lottie.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Protobuf/Protobuf.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/leveldb-library/leveldb.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
@@ -173,8 +171,6 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${PODS_ROOT}/../../AntViewer_ios/MyFrameworks/AntViewerExt.framework"
   install_framework "${PODS_ROOT}/../../AntViewer_ios/MyFrameworks/Lottie.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Protobuf/Protobuf.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/leveldb-library/leveldb.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
